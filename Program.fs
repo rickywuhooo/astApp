7434a7519ae594c514a6b0cc9cc55748d8e364e9
Ricky <120122836+rickywuhooo@users.noreply.github.com>
2025-05-07 15:38:21 -0500

    Clone professor's project

--- Program.fs
+++ Program.fs
@@ -1,82 +1,38 @@
-module astApp.App
-
-open System
-open System.IO
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Cors.Infrastructure
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
-open Microsoft.Extensions.DependencyInjection
 open Giraffe
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Hosting
 
-// ---------------------------------
-// Web app
-// ---------------------------------
-
-let webApp =
-    choose [
-        GET >=>
-            choose [
-                route "/" >=> htmlFile "WebRoot/index.html"
-            ]
-        setStatusCode 404 >=> text "Not Found" ]
-
-// ---------------------------------
-// Error handler
-// ---------------------------------
-
-let errorHandler (ex : Exception) (logger : ILogger) =
-    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
-    clearResponse >=> setStatusCode 500 >=> text ex.Message
-
-// ---------------------------------
-// Config and Main
-// ---------------------------------
-
-let configureCors (builder : CorsPolicyBuilder) =
-    builder
-        .WithOrigins(
-            "http://localhost:5000",
-            "https://localhost:5001")
-       .AllowAnyMethod()
-       .AllowAnyHeader()
-       |> ignore
-
-let configureApp (app : IApplicationBuilder) =
-    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
-    (match env.IsDevelopment() with
-    | true  ->
-        app.UseDeveloperExceptionPage()
-    | false ->
-        app .UseGiraffeErrorHandler(errorHandler)
-            .UseHttpsRedirection())
-        .UseCors(configureCors)
-        .UseStaticFiles()
-        .UseGiraffe(webApp)
-
-let configureServices (services : IServiceCollection) =
-    services.AddCors()    |> ignore
-    services.AddGiraffe() |> ignore
-
-let configureLogging (builder : ILoggingBuilder) =
-    builder.AddConsole()
-           .AddDebug() |> ignore
+open Parse
+open Fun
 
 [<EntryPoint>]
 let main args =
-    let contentRoot = Directory.GetCurrentDirectory()
-    let webRoot     = Path.Combine(contentRoot, "WebRoot")
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .UseContentRoot(contentRoot)
-                    .UseWebRoot(webRoot)
-                    .Configure(Action<IApplicationBuilder> configureApp)
-                    .ConfigureServices(configureServices)
-                    .ConfigureLogging(configureLogging)
-                    |> ignore)
-        .Build()
-        .Run()
-    0
+    let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddGiraffe() |> ignore
+
+    let app = builder.Build()
+    app.UseStaticFiles() |> ignore
+
+
+    // app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    let webApp =
+        choose [
+            route "/" >=> htmlFile "./wwwroot/index.html"
+            // your other routes
+        ]
+    app.UseGiraffe(webApp)
+
+    printfn "%A" Parse.e1
+
+    printfn "%A" Parse.e2
+
+    eval Parse.e1 [] |> printfn "%A"    
+    eval Parse.e2 [] |> printfn "%A"
+
+    print Parse.e1 |> printfn "%A"
+    print Parse.e2 |> printfn "%A"
+
+    app.Run()
+
+    0 // Exit code
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # astApp
 
 Project 3 for CS 510 Programming Language Concepts. Creates a web app that takes user input in the form of micro meta language, parses it, and creates an abstract syntax tree.
+
+# jsSyntaxTree
+_jsSyntaxTree_ is a web application that creates syntax tree graphs 
+from phrases entered in labelled bracket notation. jsSyntaxTree 
+generated graphs can be used in linguistic homework, assignments 
+and other documents.
+
+See the LICENSE file for license information.
+
+_jsSyntaxTree_ can be used on the web, without installing any 
+software at http://ironcreek.net/syntaxtree .
+
+## Support, Questions etc.
+If you encounter any bugs or problems, please file a bug report
+on the jsSyntaxTree project page:
+http://github.com/int2str/jssyntaxtree
+
+Or, send email to andre@ironcreek.net
